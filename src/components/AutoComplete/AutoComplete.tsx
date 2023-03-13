174f4e9b943f278387052a7baf26ef0a95bc4814
Breno Lemos <brenomiros@gmail.com>
2023-03-13 19:41:31 -0300

    feat: get the initial pokemon values

--- src/components/AutoComplete/AutoComplete.tsx
+++ src/components/AutoComplete/AutoComplete.tsx
@@ -1,5 +1,76 @@
+import { useEffect, useState } from "react";
+
 const AutoComplete = () => {
-  return <h1>Auto Complete</h1>;
+  const [display, setDisplay] = useState(false);
+  const [options, setOptions] = useState<
+    {
+      name: string;
+      sprite: string;
+    }[]
+  >([]);
+  const [search, setSearch] = useState("");
+
+  const setActivePokemon = (pokemon: any) => {
+    setSearch(pokemon.name);
+    setDisplay(false);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const pokemon: {
+        name: string;
+        sprite: string;
+      }[] = [];
+
+      const promises = new Array(20)
+        .fill(0)
+        .map((v, i) =>
+          fetch(`https://pokeapi.co/api/v2/pokemon-form/${i + 1}`),
+        );
+
+      Promise.all(promises).then((pokemonArr) => {
+        return pokemonArr.map((value) =>
+          value
+            .json()
+            .then(({ name, sprites: { front_default: sprite } }) =>
+              pokemon.push({ name, sprite }),
+            ),
+        );
+      });
+      // console.log(pokemon);
+      console.log(pokemon);
+      setOptions(pokemon);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <h1>Auto Complete</h1>
+
+      <input
+        id="auto"
+        type="text"
+        onClick={() => setDisplay(!display)}
+        placeholder="Loook for a Pokemon..."
+      />
+
+      {display && (
+        <div>
+          {options.map((option, index) => (
+            <div
+              key={`pokemon-${index}`}
+              onClick={() => setActivePokemon(option)}
+            >
+              <span>{option.name}</span>
+              <img src={option.sprite} alt={option.name} />
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
 };
 
 export default AutoComplete;
