48a90cd901e12d3a065f2f3abfed972db4d3c2b6
Breno Lemos <brenomiros@gmail.com>
2023-03-13 21:37:38 -0300

    refactor: split the logic into functions

--- src/components/AutoComplete/AutoComplete.tsx
+++ src/components/AutoComplete/AutoComplete.tsx
@@ -1,25 +1,43 @@
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 
 import styles from "./AutoComplete.module.css";
 
 const AutoComplete = () => {
-  const [filteredData, setFilteredData] = useState([]);
-  const [display, setDisplay] = useState(false);
+  const [filteredData, setFilteredData] = useState<
+    {
+      name: string;
+      sprite: string;
+    }[]
+  >([]);
   const [options, setOptions] = useState<
     {
       name: string;
       sprite: string;
     }[]
   >([]);
-  const [search, setSearch] = useState("");
+  const [enteredWord, setEnteredWord] = useState("");
 
-  const setActivePokemon = (pokemon: any) => {
-    setSearch(pokemon.name);
-    setDisplay(false);
+  const handleFilteredList = (filterItem: string) => {
+    const newFilter = options.filter((option) =>
+      option.name.toLowerCase().includes(filterItem.toLowerCase()),
+    );
+
+    if (filterItem === "") return setFilteredData([]);
+
+    setFilteredData(newFilter);
   };
 
-  const handlefilter = (event) => {
+  const setActiveItem = (item: string) => {
+    setEnteredWord(item);
+
+    handleFilteredList(item);
+  };
+
+  const handlefilter = (event: ChangeEvent<HTMLInputElement>) => {
     const searchWord = event.target.value;
+    setEnteredWord(searchWord);
+
+    handleFilteredList(searchWord);
   };
 
   useEffect(() => {
@@ -44,8 +62,7 @@ const AutoComplete = () => {
             ),
         );
       });
-      // console.log(pokemon);
-      console.log(pokemon);
+
       setOptions(pokemon);
     };
 
@@ -59,35 +76,32 @@ const AutoComplete = () => {
       <input
         id="auto"
         type="text"
-        onClick={() => setDisplay(!display)}
         placeholder="Loook for a Pokemon..."
-        value={search}
-        onChange={(event) => setSearch(event.target.value)}
+        value={enteredWord}
+        onChange={handlefilter}
         className={styles.searchInput}
       />
 
       {filteredData.length !== 0 && (
         <div className={styles.dataResult}>
-          {options
-            .filter(({ name }) => !name.indexOf(search.toLowerCase()))
-            .map((option, index) => (
-              <div
-                key={`pokemon-${index}`}
-                onClick={() => setActivePokemon(option)}
-                tabIndex={0}
-                className={styles["dataResult__item"]}
-              >
-                <span className={styles["dataResult__item__text"]}>
-                  {option.name}
-                </span>
+          {filteredData.map((option, index) => (
+            <div
+              key={`pokemon-${index}`}
+              onClick={() => setActiveItem(option.name)}
+              tabIndex={0}
+              className={styles["dataResult__item"]}
+            >
+              <span className={styles["dataResult__item__text"]}>
+                {option.name}
+              </span>
 
-                <img
-                  className={styles["dataResult__item__image"]}
-                  src={option.sprite}
-                  alt={option.name}
-                />
-              </div>
-            ))}
+              <img
+                className={styles["dataResult__item__image"]}
+                src={option.sprite}
+                alt={option.name}
+              />
+            </div>
+          ))}
         </div>
       )}
     </div>
